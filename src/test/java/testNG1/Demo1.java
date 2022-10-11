2eab3900b059ba6fad93134b32964a3fc67e861c
sandy0518 <sai.sandeep.906@gmail.com>
2022-10-11 13:33:28 +0530

    Added testNG1 Package

---
+++ src/test/java/testNG1/Demo1.java
@@ -0,0 +1,16 @@
+package testNG1;
+
+import org.testng.annotations.Test;
+
+public class Demo1 extends Annotations{
+	
+	@Test
+	public void test1() {
+		System.out.println("Demo");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Demo2");
+	}
+}
